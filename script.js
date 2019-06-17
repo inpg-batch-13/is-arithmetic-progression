d51d10e8961909ff92f49198dfa49b2b7f50cc03
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-06-17 23:25:23 +0700

    dif

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function isArithmeticProgression (numbers) {
   // Your code here
+  var dif = (numbers[1]) - (numbers[0])
+  for(var i = 0; i < numbers.length - 1; i++){
+  var dif2 = numbers[i+1] - numbers[i] 
+    if (dif != dif2){
+      return false 
+    } 
+  }
+  return true
 }
 
 // TEST CASES
 console.log(isArithmeticProgression([1, 2, 3, 4, 5, 6])); // true
-console.log(isArithmeticProgression([2, 4, 6, 12, 24])); // false
+console.log(isArithmeticProgression([2, 4, 6, 12, 24,26])); // false
 console.log(isArithmeticProgression([2, 4, 6, 8])); // true
 console.log(isArithmeticProgression([2, 6, 18, 54])); // false
 console.log(isArithmeticProgression([1, 2, 3, 4, 7, 9])); // false
