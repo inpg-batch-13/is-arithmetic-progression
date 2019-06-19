f43452f958f9e3b45b9cec4ad431e3499cce8420
Rusydy <rusydymuhiddin@gmail.com>
2019-06-19 18:25:23 +0700

    sudah tapi masih bingung, enggak yakin

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function isArithmeticProgression (numbers) {
-  // Your code here
-}
-
+function isArithmeticProgression (numbers) 
+{
+  var dif1 = (numbers[1]) - (numbers[0])
+  for (var i = 0 ; i < numbers.length - 1 ; i++)
+  {
+    var dif2 = numbers [i+1] - numbers [i]
+    if (dif1 != dif2) {
+      return false }
+    }
+  }
+      return true
 // TEST CASES
 console.log(isArithmeticProgression([1, 2, 3, 4, 5, 6])); // true
 console.log(isArithmeticProgression([2, 4, 6, 12, 24])); // false
